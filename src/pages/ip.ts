a39f380f7fd31470e3b4765a815771d0987d822a
LePepe <josepindy@gmail.com>
2025-01-14 17:04:19 -0300

    fix(ip.ts): simplify response to return clientAddress directly instead of wrapping it in a JSON object for clarity and efficiency

--- src/pages/ip.ts
+++ src/pages/ip.ts
@@ -3,8 +3,6 @@ import type {APIRoute} from "astro";
 export const GET: APIRoute = ({ clientAddress }) => {
     console.log(clientAddress)
     return new Response(
-        JSON.stringify({
-            ip: clientAddress
-        })
+        clientAddress
     )
 }
