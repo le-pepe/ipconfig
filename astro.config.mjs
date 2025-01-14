17a393686c9c7cffa5dd88acd8a49d85302eb142
LePepe <josepindy@gmail.com>
2025-01-14 13:35:02 -0300

    feat(config): set output option to 'server' in astro.config.mjs for server-side rendering support style(index.astro): remove redundant class name 'w-full' from table element for cleaner markup

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,7 +8,7 @@ import node from '@astrojs/node';
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
-
+  output: 'server',
   adapter: node({
     mode: 'standalone'
   })
