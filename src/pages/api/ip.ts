b033c9a65d70558374b6613ab30bf08a9d7fcf32
LePepe <josepindy@gmail.com>
2025-01-14 15:28:58 -0300

    feat(layout): update page title from "Astro Basics" to "My IP" for better relevance fix(ip.ts): log clientAddress to console for debugging purposes in the IP API route

--- src/pages/api/ip.ts
+++ src/pages/api/ip.ts
@@ -1,7 +1,7 @@
 import type {APIRoute} from "astro";
 
 export const GET: APIRoute = ({ clientAddress }) => {
-
+    console.log(clientAddress)
     return new Response(
         JSON.stringify({
             ip: clientAddress
